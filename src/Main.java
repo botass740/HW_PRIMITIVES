a071f972e11f791c089e86f926f87d16d57ea52a
Max test <botass740@gmail.com>
2023-04-14 20:54:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 23000;
+
+        int bonusMile = ticketPrice / 20;
+
+        System.out.println("Начислено бонусов - " + bonusMile);
+    }
+}
